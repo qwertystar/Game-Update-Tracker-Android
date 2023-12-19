c40c80d44194979877b219ac61588be0dd4d747d
qwertystar <qwertystarprog@sina.cn>
2023-12-20 00:33:29 +0800

    优化apk生成流程

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,22 +11,28 @@ android {
         applicationId = "com.qwertystar.gameupdatetracker"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
+        setProperty("archivesBaseName", "Game Update Tracker-v${versionName}")
     }
 
     buildTypes {
-        release {
+        debug {
             isMinifyEnabled = false
+        }
+        release {
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+
         }
     }
     compileOptions {
