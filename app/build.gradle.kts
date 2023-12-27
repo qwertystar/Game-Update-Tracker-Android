b8d1f20f5df82dfd5ad74a1ff528ccf6d7e4a4aa
qwertystar <qwertystarprog@sina.cn>
2023-12-28 00:25:53 +0800

    just change versionNo of app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.qwertystar.gameupdatetracker"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
